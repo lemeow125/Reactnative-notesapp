958e62d733e19813e76a2f9d5d49d46ac1eebbc2
toledo <kurtzidanetoledo@gmail.com>
2023-04-08 16:24:15 +0800

    Polished apis

--- src/Components/Api/Api.tsx
+++ src/Components/Api/Api.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   ActivationParams,
   UpdateNoteParams,
@@ -13,10 +14,10 @@ const instance = axios.create({
   baseURL: "https://keannu126.pythonanywhere.com",
 });
 
-export function GetNotes() {
-  const token = JSON.parse(localStorage.getItem("token") || "{}");
+export async function GetNotes() {
+  const token = JSON.parse(await AsyncStorage.getItem("token") || "{}");
   return instance
-    .get("api/v1/notes/", {
+    .get("/api/v1/notes/", {
       headers: {
         Authorization: "Token " + token,
       },
@@ -26,10 +27,16 @@ export function GetNotes() {
     });
 }
 
-export function GetNote(id: number) {
-  const token = JSON.parse(localStorage.getItem("token") || "{}");
+export function GetPublicNotes() {
+  return instance.get("/api/v1/public_notes/").then((response) => {
+    return response.data;
+  });
+}
+
+export async function GetNote(id: number) {
+  const token = JSON.parse(await AsyncStorage.getItem("token") || "{}");
   return instance
-    .get("api/v1/notes/" + id + "/", {
+    .get("/api/v1/notes/" + id + "/", {
       headers: {
         Authorization: "Token " + token,
       },
@@ -39,10 +46,10 @@ export function GetNote(id: number) {
     });
 }
 
-export function UpdateNote(note: UpdateNoteParams) {
-  const token = JSON.parse(localStorage.getItem("token") || "{}");
+export async function UpdateNote(note: UpdateNoteParams) {
+  const token = JSON.parse(await AsyncStorage.getItem("token") || "{}");
   return instance
-    .patch("api/v1/notes/" + note.id + "/", note, {
+    .patch("/api/v1/notes/" + note.id + "/", note, {
       headers: {
         Authorization: "Token " + token,
       },
@@ -51,15 +58,14 @@ export function UpdateNote(note: UpdateNoteParams) {
       return response.data;
     })
     .catch((error) => {
-      console.log("Error updating note", error);
       return error;
     });
 }
 
-export function AddNote(note: AddNoteParams) {
-  const token = JSON.parse(localStorage.getItem("token") || "{}");
+export async function AddNote(note: AddNoteParams) {
+  const token = JSON.parse(await AsyncStorage.getItem("token") || "{}");
   return instance
-    .post("api/v1/notes/", note, {
+    .post("/api/v1/notes/", note, {
       headers: {
         Authorization: "Token " + token,
       },
@@ -68,21 +74,19 @@ export function AddNote(note: AddNoteParams) {
       return response.data;
     })
     .catch((error) => {
-      console.log("Error adding note", error);
       return error;
     });
 }
 
-export function DeleteNote(id: number) {
-  const token = JSON.parse(localStorage.getItem("token") || "{}");
+export async function DeleteNote(id: number) {
+  const token = JSON.parse(await AsyncStorage.getItem("token") || "{}");
   return instance
-    .delete("api/v1/notes/" + id + "/", {
+    .delete("/api/v1/notes/" + id + "/", {
       headers: {
         Authorization: "Token " + token,
       },
     })
     .catch((error) => {
-      console.log("Error deleting note", error);
       return error;
     });
 }
@@ -91,26 +95,20 @@ export function DeleteNote(id: number) {
 
 export function UserRegister(register: RegistrationParams) {
   return instance
-    .post("api/v1/accounts/users/", register)
+    .post("/api/v1/accounts/users/", register)
     .then(async (response) => {
-      console.log(response.data);
       return true;
     })
     .catch((error) => {
-      console.log("Registration failed: " + error);
       return false;
     });
 }
 
 export function UserLogin(user: LoginParams) {
   return instance
-    .post("api/v1/accounts/token/login/", user)
+    .post("/api/v1/accounts/token/login/", user)
     .then(async (response) => {
-      localStorage.setItem("token", JSON.stringify(response.data.auth_token));
-      console.log(
-        "Login Success! Stored Token: ",
-        JSON.parse(localStorage.getItem("token") || "{}")
-      );
+      AsyncStorage.setItem("token", JSON.stringify(response.data.auth_token));
       return true;
     })
     .catch((error) => {
@@ -119,29 +117,26 @@ export function UserLogin(user: LoginParams) {
     });
 }
 
-export function UserInfo() {
-  const token = JSON.parse(localStorage.getItem("token") || "{}");
+export async function UserInfo() {
+  const token = JSON.parse(await AsyncStorage.getItem("token") || "{}");
   return instance
-    .get("api/v1/accounts/users/me/", {
+    .get("/api/v1/accounts/users/me/", {
       headers: {
         Authorization: "Token " + token,
       },
     })
     .then((response) => {
-      console.log(response.data);
       return response.data;
     });
 }
 
 export function UserActivate(activation: ActivationParams) {
   return instance
-    .post("api/v1/accounts/users/activation/", activation)
+    .post("/api/v1/accounts/users/activation/", activation)
     .then(async (response) => {
-      console.log("Activation Success");
       return true;
     })
     .catch((error) => {
-      console.log("Activation failed: " + error);
       return false;
     });
 }
--- src/Features/Redux/Store/Store.tsx
+++ src/Features/Redux/Store/Store.tsx
@@ -2,15 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import LoginReducer from "../Slices/LoginSlice/LoginSlice";
 import LoggedInUserReucer from "../Slices/LoggedInUserSlice/LoggedInUserSlice";
 
-const store = configureStore({
+export default configureStore({
   reducer: {
-    logged_in: LoginReducer,
-    logged_in_user: LoggedInUserReucer,
+    Login: LoginReducer,
+    LoggedInUser: LoggedInUserReucer,
   },
 });
-
-export default store;
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
