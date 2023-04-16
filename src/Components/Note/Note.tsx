c2f59420534fce7bc88f7a1b2c208e84c0ed94d4
keannu125 <noehbernasol0@gmail.com>
2023-04-16 18:48:29 +0800

    Added delete note functionality

--- src/Components/Note/Note.tsx
+++ src/Components/Note/Note.tsx
@@ -2,8 +2,19 @@ import * as React from "react";
 import { View, Text, TextInput, ScrollView } from "react-native";
 import styles from "../../styles";
 import { NoteProps } from "../../Interfaces/Interfaces";
+import ButtonCentered from "../Buttons/ButtonCentered/ButtonCentered";
+import { useQueryClient, useMutation } from "react-query";
+import { DeleteNote } from "../Api/Api";
 
 export default function Note(props: NoteProps) {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: DeleteNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries("notes");
+      queryClient.invalidateQueries("public_notes");
+    },
+  });
   return (
     <View style={styles.addnotecont}>
       <View style={styles.tle}>
@@ -18,6 +29,29 @@ export default function Note(props: NoteProps) {
         <ScrollView style={styles.typeinput} nestedScrollEnabled={true}>
           <Text style={styles.typeinput}>{props.content}</Text>
         </ScrollView>
+      </View>
+      <View style={styles.flex_row}>
+        <ButtonCentered
+          color={"Red"}
+          onPress={() => {
+            console.log("Deleted note id " + props.id);
+            mutation.mutate(props.id);
+          }}
+          width={64}
+        >
+          <Text style={{ ...styles.text_white, ...{ fontSize: 16 } }}>
+            Delete Note
+          </Text>
+        </ButtonCentered>
+        <ButtonCentered
+          color={"Yellow"}
+          onPress={() => console.log("Edited note id " + props.id)}
+          width={64}
+        >
+          <Text style={{ ...styles.text_white, ...{ fontSize: 16 } }}>
+            Edit Note
+          </Text>
+        </ButtonCentered>
       </View>
     </View>
   );
---
+++ src/Routes/Note/Note.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function Note(id: number) {
+  return (
+    <View>
+      <Text>Note</Text>
+    </View>
+  );
+}
