c782851c487bfc1ed8e6234f7373d6285f029add
keannu125 <noehbernasol0@gmail.com>
2023-04-16 18:06:08 +0800

    Fixed login page referencing wrong file for redux login state and made sidebar responsive to login state

--- src/Components/Drawer/DrawerScreenSettings/CustomDrawerContent/CustomDrawerContent.tsx
+++ src/Components/Drawer/DrawerScreenSettings/CustomDrawerContent/CustomDrawerContent.tsx
@@ -14,98 +14,136 @@ import SignupIcon from "../../../Icons/SignupIcon/SignupIcon";
 import UserIcon from "../../../Icons/UserIcon/UserIcon";
 import AppIcon from "../../../Icons/AppIcon/AppIcon";
 import LogoutIcon from "../../../Icons/LogoutIcon/LogoutIcon";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../Features/Redux/Store/Store";
 
 export default function CustomDrawerContent(props: {}) {
   const navigation = useNavigation<RootDrawerParamList>();
   const width = 224;
-  return (
-    <DrawerContentScrollView {...props}>
-      <View
-        style={{
-          ...styles.flex_row,
-          ...{ justifyContent: "center" },
-        }}
-      >
-        <AppIcon size={32} color="white" />
-        <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
-          Clip Notes
-        </Text>
-      </View>
-
-      <ButtonAlignLeft
-        color="Blue"
-        width={width}
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
-      >
-        <HomeIcon size={32} color="white" />
-        <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>Home</Text>
-      </ButtonAlignLeft>
-
-      <ButtonAlignLeft
-        color="Green"
-        width={width}
-        onPress={() => {
-          navigation.navigate("New Note");
-        }}
-      >
-        <AddIcon size={32} color="white" />
-        <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
-          New Note
-        </Text>
-      </ButtonAlignLeft>
-
-      <ButtonAlignLeft
-        color="Green"
-        width={width}
-        onPress={() => {
-          navigation.navigate("Login");
-        }}
-      >
-        <LoginIcon size={32} color="white" />
-        <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>Login</Text>
-      </ButtonAlignLeft>
-
-      <ButtonAlignLeft
-        color="Yellow"
-        width={width}
-        onPress={() => {
-          navigation.navigate("User Info");
-        }}
-      >
-        <UserIcon size={32} color="white" />
-        <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
-          User Info
-        </Text>
-      </ButtonAlignLeft>
-
-      <ButtonAlignLeft
-        color="Yellow"
-        width={width}
-        onPress={() => {
-          navigation.navigate("Register");
-        }}
-      >
-        <SignupIcon size={32} color="white" />
-        <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
-          Register
-        </Text>
-      </ButtonAlignLeft>
-
-      <ButtonAlignLeft
-        color="Red"
-        width={width}
-        onPress={() => {
-          navigation.navigate("Register");
-        }}
-      >
-        <LogoutIcon size={32} color="white" />
-        <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
-          Log Out
-        </Text>
-      </ButtonAlignLeft>
-
-    </DrawerContentScrollView>
+  const logged_in = useSelector((state: RootState) => state.logged_in.value);
+  const logged_in_user = useSelector(
+    (state: RootState) => state.logged_in_user.value
   );
+  if (logged_in) {
+    return (
+      <DrawerContentScrollView {...props}>
+        <View
+          style={{
+            ...styles.flex_row,
+            ...{ justifyContent: "center" },
+          }}
+        >
+          <AppIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            Clip Notes
+          </Text>
+        </View>
+        <Text style={{ ...styles.text_white, ...{ fontSize: 16 } }}>
+          Logged in as {logged_in_user.username}
+        </Text>
+        <ButtonAlignLeft
+          color="Blue"
+          width={width}
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <HomeIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            Home
+          </Text>
+        </ButtonAlignLeft>
+
+        <ButtonAlignLeft
+          color="Green"
+          width={width}
+          onPress={() => {
+            navigation.navigate("New Note");
+          }}
+        >
+          <AddIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            New Note
+          </Text>
+        </ButtonAlignLeft>
+        <ButtonAlignLeft
+          color="Yellow"
+          width={width}
+          onPress={() => {
+            navigation.navigate("User Info");
+          }}
+        >
+          <UserIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            User Info
+          </Text>
+        </ButtonAlignLeft>
+        <ButtonAlignLeft
+          color="Red"
+          width={width}
+          onPress={() => {
+            navigation.navigate("Register");
+          }}
+        >
+          <LogoutIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            Log Out
+          </Text>
+        </ButtonAlignLeft>
+      </DrawerContentScrollView>
+    );
+  } else {
+    return (
+      <DrawerContentScrollView {...props}>
+        <View
+          style={{
+            ...styles.flex_row,
+            ...{ justifyContent: "center" },
+          }}
+        >
+          <AppIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            Clip Notes
+          </Text>
+        </View>
+
+        <ButtonAlignLeft
+          color="Blue"
+          width={width}
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <HomeIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            Home
+          </Text>
+        </ButtonAlignLeft>
+        <ButtonAlignLeft
+          color="Green"
+          width={width}
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        >
+          <LoginIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            Login
+          </Text>
+        </ButtonAlignLeft>
+        <ButtonAlignLeft
+          color="Yellow"
+          width={width}
+          onPress={() => {
+            navigation.navigate("Register");
+          }}
+        >
+          <SignupIcon size={32} color="white" />
+          <Text style={{ ...styles.text_white, ...{ fontSize: 32 } }}>
+            Register
+          </Text>
+        </ButtonAlignLeft>
+      </DrawerContentScrollView>
+    );
+  }
 }
